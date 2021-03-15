a046fd11aca4a68e69e724923a2e97c0324a7a9b
Pooria <pjrashidi@gmail.com>
2021-03-14 23:12:05 -0600

    added some letters that can spawn

--- scripts.js
+++ scripts.js
@@ -2,17 +2,16 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d'); 
 ctx.fillStyle = 'white'
 
-const scale = 100;
-const width = scale;
-const height = scale;
+const width = 150;
+const height = 150;
 
 const grid = new Array(height)
 for(let y = 0; y < height; y++) {
   grid[y] = (new Array(width))
 }
 
-const tickInterval = window.setInterval(tick, 30);
-window.setTimeout(() => {window.clearInterval(tickInterval)}, 50000)
+const tickInterval = window.setInterval(tick, 100);
+// window.setTimeout(() => {window.clearInterval(tickInterval)}, 2000)
 
 function shiftSand(){
   let y = height - 1
@@ -36,27 +35,21 @@ function shiftGrain(x,y){
 
 function randomShift(x,y){
   let options = []
-  if (grid[x , y + 1] != 1) { options.push([x , y + 1]) }
+  if (grid[x , y + 1] != 1) { 
+    _.times(10, () => { options.push([x , y + 1]) })
+  }
   if (grid[x + 1, y + 1] != 1) { options.push([x + 1, y + 1]) }
   if (grid[x - 1, y + 1] != 1) { options.push([x - 1, y + 1]) }
 
-  randomPick = options[getRandomInt(options.length)]
+  randomPick = options[_.random(options.length - 1)]
 
+  console.log(randomPick)
   grid[randomPick[1]][randomPick[0]] = 1
   grid[y][x] = null
 }
 
 function hasGrainsUnder(x,y) {
   return grid[y + 1][x] && grid[y + 1][x + 1] && grid[y + 1][x - 1]
-}
-
-function randomHorizontalShift(){
-  const options = [-1,0,0,0,0,0,1]
-  return options[getRandomInt(options.length)]
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
 }
 
 function clearCanvas(){
@@ -67,10 +60,14 @@ function drawSand(){
   for(let x = 0; x < width; x++) {
     for(let y = 0; y < height; y++) {
       if (grid[y][x]) {
-        ctx.fillRect(x,y, 1, 1);
+        ctx.fillRect(x, y, 1, 1);
       }
     }
   }
+}
+
+function paint(event) {
+  grid[event.layerY][event.layerX] = 1
 }
 
 function addSand(x,y){
@@ -78,11 +75,28 @@ function addSand(x,y){
 }
 
 function tick(){
-  console.log(grid);
   //update model
-  addSand(width * .5, height * .1)
   shiftSand()
   //update view
   clearCanvas()
   drawSand()
+}
+
+canvas.addEventListener('click', function(event) {
+  paint(event)
+});
+
+window.addEventListener('keypress', handleKeyPress)
+
+function handleKeyPress(e){
+  const offset = width / 2
+  console.log(keys[e.key])
+  if (keys[e.key]){
+    keys[e.key].forEach((row, y) => {
+      row.forEach((point, x) => {
+        grid[y + 10][x + offset] = point
+      })
+    })
+    drawSand()
+  }
 }
